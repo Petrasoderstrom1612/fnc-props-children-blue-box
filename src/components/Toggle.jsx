f10f6d8bf442c646bd12bcfbb19fad93621bebc7
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-08-28 09:52:31 +0200

    Toggle created with state, context, useeffect passing value for provider, checking use effect with on to either toggle or use reft and lastly passing children further

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import useRef from "react"
+
+const ContextToggle = React.createContext()
+
+const Toggle = () => {
+    const [on, setOn] = React.useState(false)
+    const nonFirstRenderRef = useRef(true)
+
+    const toggle = () => {
+        setOn(prevOn => !prevOn)
+    }
+
+    React.useEffect(()=>{
+        if(nonFirstRenderRef.current){
+            nonFirstRenderRef.current = false
+        } else {
+            toggle()
+        }
+    },[on])
+
+  return (                  //do not forget .Provider 
+    <ContextToggle.Provider value={{on, toggle}}> 
+        {children}
+    </ContextToggle.Provider>
+  )
+}
+
+export default Toggle
+export {ContextToggle}
